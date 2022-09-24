1ccd5247fdd091521d58a0c997ab4bc2d1108d15
JiayingCui <15305187249@gts.work>
2022-09-23 20:44:13 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // selector - html tag
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// propery of the componet
+// method
 export class AppComponent {
-  title = 'my-app';
+  
 }
